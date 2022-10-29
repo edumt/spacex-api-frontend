26033058063ad232b169d8e0a0ce141d148e1fb3
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-29 15:46:25 -0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col justify-between h-full">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="p-4 flex justify-center gap-4 text-sm">
+      This website is not affiliated with SpaceX™.
+      <a href="#">GitHub</a>
+      <a href="#">LinkedIn</a>
+    </footer>
+  );
+};
+
+export default Footer;
