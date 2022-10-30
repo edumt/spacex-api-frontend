1f364909e67ec4188d0d5a28963b3226d0fdb6c5
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-30 20:37:25 -0300

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex flex-col justify-between h-full">
+    <div className="flex flex-col justify-between bg-rocket-mobile md:bg-rocket bg-no-repeat bg-cover bg-center bg-fixed">
       <Header />
       <Footer />
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        rocket: "url('/images/rocket.jpg')",
+        "rocket-mobile": "url('/images/rocket-mobile.jpg')",
+      },
+    },
   },
   plugins: [],
 };
