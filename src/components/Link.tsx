834ba7a241b132f2af2a7e2a04b781d15dfda197
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-30 22:38:32 -0300

    update links color

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,7 +3,7 @@ const Link: React.FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = (
 ) => {
   return (
     <a
-      className="text-blue-600 hover:text-blue-800 hover:underline"
+      className="text-blue-200 hover:text-blue-50 hover:underline"
       {...props}
     />
   );
