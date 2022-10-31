9ee00f9f4afd2a78795c2c3ddd15225b9fb7b081
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-30 23:31:19 -0300

    remove links from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,11 @@
 import logo from "../assets/spacex-logo.png";
-import Link from "./Link";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-evenly py-4 px-8 items-end">
-      <img src={logo} className="h-8" />
-      <nav className="flex gap-4">
-        {/* to do: melhorar a chamada */}
-        Launches:
-        <Link href="#">Next</Link>
-        <Link href="#">Last</Link>
-        <Link href="#">Upcoming</Link>
-        <Link href="#">Past</Link>
-      </nav>
+    <header className="mx-auto py-4 px-8 brightness-125">
+      <a href="/">
+        <img src={logo} className="h-8" alt="SpaceX Logo" />
+      </a>
     </header>
   );
 };
