e3b00b2c6d9dcf8d26ae9c06814d93bd2ed65a62
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-30 22:38:23 -0300

    add contrast and responsiveness to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,13 @@ import Link from "./Link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="p-4 flex justify-center gap-4 text-sm">
+    <footer className="flex flex-col items-center justify-center gap-4 bg-black/50 p-4 text-sm text-gray-50 md:flex-row">
       This website is not affiliated with SpaceX™.
-      <Link href="#">GitHub</Link>
-      <Link href="#">LinkedIn</Link>
+      <nav className="flex gap-4">
+        <Link href="https://www.spacex.com/">Website</Link>
+        <Link href="https://www.flickr.com/photos/spacex/">Flickr</Link>
+        <Link href="https://twitter.com/SpaceX">Twitter</Link>
+      </nav>
     </footer>
   );
 };
