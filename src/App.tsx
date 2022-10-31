adfffe63d595b09dbefc2425a4f82508e442ea88
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-31 00:36:49 -0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
-import Home from "./pages/Home";
 import RelatedLinks from "./layout/RelatedLinks";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="flex flex-col justify-between bg-rocket-mobile bg-cover bg-fixed bg-center bg-no-repeat md:bg-rocket">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
       <RelatedLinks />
       <Footer />
     </div>
