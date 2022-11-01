f05607fb842f3ae4d542bfd56cccedfdf323e6a4
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-11-01 12:34:43 -0300

    improved readability on small screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div className="h-full text-white">
       <section className="flex flex-col">
-        <div className="mt-10 flex h-48 items-end p-4 md:ml-24 md:w-2/4 xl:ml-48 xl:w-1/3">
+        <div className="mt-10 flex h-48 items-end bg-black/50 p-4 md:ml-24 md:w-2/4 xl:ml-48 xl:w-1/3">
           SpaceX designs, manufactures and launches advanced rockets and
           spacecraft. The company was founded in 2002 to revolutionize space
           technology, with the ultimate goal of enabling people to live on other
