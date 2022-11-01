75fa1ba9c0edc37c9f48aabc9ee6d7debc233b40
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-31 23:52:44 -0300

    target _blank on footer links

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -5,9 +5,15 @@ const Footer: React.FC = () => {
     <footer className="flex flex-col items-center justify-center gap-4 bg-black/50 p-4 text-sm text-gray-50 md:flex-row">
       This website is not affiliated with SpaceX™.
       <nav className="flex gap-4">
-        <Link href="https://www.spacex.com/">Website</Link>
-        <Link href="https://www.flickr.com/photos/spacex/">Flickr</Link>
-        <Link href="https://twitter.com/SpaceX">Twitter</Link>
+        <Link href="https://www.spacex.com/" target="_blank">
+          Website
+        </Link>
+        <Link href="https://www.flickr.com/photos/spacex/" target="_blank">
+          Flickr
+        </Link>
+        <Link href="https://twitter.com/SpaceX" target="_blank">
+          Twitter
+        </Link>
       </nav>
     </footer>
   );
